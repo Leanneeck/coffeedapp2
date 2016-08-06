0bc8b765fff6cdf175d577d3a1ff69dfb01a3dc5
Leanne Eckelberg <leanne.eckelberg@gmail.com>
2016-08-05 19:08:04 -0600

    changed STATIC_URL path

--- coffeedapp2/settings.py
+++ coffeedapp2/settings.py
@@ -135,15 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
-STATICFILES_DIRS = (
-    os.path.join(MAIN_DIR, 'coffeedapp2', 'static'),
-    )
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(MAIN_DIR, 'coffeedapp2', 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(MAIN_DIR, '/static/')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
